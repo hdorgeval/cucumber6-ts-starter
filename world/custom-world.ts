c03db38a31f5c3a0fabd77140078f6937337cfb2
hdorgeval <henri.dorgeval@outlook.fr>
2019-11-09 19:16:57 +0100

    fix(world): clean code

--- world/custom-world.ts
+++ world/custom-world.ts
@@ -7,18 +7,20 @@ export type MediaType = 'text/plain' | 'image/png' | 'application/json';
 export type AttachBuffer = (data: Buffer, mediaType: MediaType) => void;
 export type AttachStream = (data: Stream, mediaType: MediaType) => void;
 export type AttachText = (data: string) => void;
-export type AttachStringiFiedJson = (data: string, mediaType: 'application/json') => void;
+export type AttachStringifiedJson = (data: string, mediaType: 'application/json') => void;
 export type AttachBase64EncodedPng = (data: string, mediaType: 'image/png') => void;
-export type AttachFn = AttachBuffer | AttachStream | AttachBase64EncodedPng | AttachText;
+export type AttachFn =
+  | AttachBuffer
+  | AttachStream
+  | AttachBase64EncodedPng
+  | AttachStringifiedJson
+  | AttachText;
 
 export interface CucumberWorldConstructorParams {
   attach: AttachFn;
   parameters: { [key: string]: string };
 }
 
-export interface CustomContext {
-  cliArgs: CliArgs;
-}
 export class CustomWorld {
   public attach: AttachFn;
   public cliArgs: CliArgs;
