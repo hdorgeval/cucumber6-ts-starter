a1962dc36ee4b374068e28629b0818856aa52c70
hdorgeval <henri.dorgeval@outlook.fr>
2020-07-26 19:27:08 +0200

    fix(steps): replace chai par jest/expect

--- step-definitions/maths/simple-maths-steps.ts
+++ step-definitions/maths/simple-maths-steps.ts
@@ -1,29 +1,21 @@
 import { SimpleMathsCalculator } from '../../domains/simple-maths/simple-maths-calculator';
-import { expect } from 'chai';
+import expect from 'expect';
 import { Before, Given, Then, When } from 'cucumber';
 
 Given('I have a simple maths calculator', async function () {
-  // Uncomment below line if no reporter is declared on the CLI
-  // this.logger.info('Given I have a simple maths calculator');
   this.calculator = new SimpleMathsCalculator();
 });
 
 Given('a variable set to {int}', async function (value: number) {
-  // Uncomment below line if no reporter is declared on the CLI
-  // this.logger.info(`Given a variable set to ${value}`);
   this.calculator.startWith(value);
 });
 
 When('I increment the variable by {int}', async function (value: number) {
-  // Uncomment below line if no reporter is declared on the CLI
-  // this.logger.info(`When I increment the variable by ${value}`);
   this.calculator.incrementBy(value);
 });
 
 Then('the variable should contain {int}', async function (value: number) {
-  // Uncomment below line if no reporter is declared on the CLI
-  // this.logger.info(`Then the variable should contain ${value}`);
-  expect(this.calculator.result).to.equals(value);
+  expect(this.calculator.result).toBe(value);
 });
 
 /**
